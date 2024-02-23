b805e7d041e06380ad11de63b3569484b659c815
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-02-23 11:55:35 +0000

    restarted project, added bootstrap to react environment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 
 import "./weather.css";
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import Weather from "./Weather";
 import Overview from "./Overview";
@@ -11,9 +12,12 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-    <><App /><div className="engulf">
+    <div className="container">
+      <App />
+      <div className="engulf">
     <Weather />
     <Overview />
     <Footer />
-  </div></>
+  </div>
+  </div>
 );
